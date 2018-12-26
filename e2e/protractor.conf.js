19f135051a298ec34e14be93eabcae5c24c901de
Siva Phaneendra Babu Dandu <sivaphaneendra@users.noreply.github.com>
2018-12-26 11:47:10 +0530

    added localhost

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://shopping-app-siva.herokuapp.com/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
